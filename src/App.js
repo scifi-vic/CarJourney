0ceb410bee2e9247c28728d0b34f3bc760ba1cc3
nicholas64chan <81445846+nicholas64chan@users.noreply.github.com>
2024-11-02 14:05:08 -0700

    car-quiz added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import SideBarDrawer from './components/SideBarDrawer';
-// import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from "./pages/Home";
-import About from "./pages/About";
-import User from "./pages/User";
-import Garage from "./pages/Garage";
-import { RiEdgeFill } from 'react-icons/ri';
+import Home from './pages/Home';
+import About from './pages/About';
+import Garage from './pages/Garage';
+import User from './pages/User';
+import CarQuiz from './pages/Car-Quiz'; // Updated import for Car-Quiz.js
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<SideBarDrawer />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/garage" element={<Garage />} />
-            <Route path="/user" element={<User />} />
-          </Route>
-        </Routes> 
-    </BrowserRouter>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/garage" element={<Garage />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/car-quiz" element={<CarQuiz />} /> {/* Route for Car Quiz */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
-export default App
+export default App;
