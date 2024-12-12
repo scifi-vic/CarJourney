0aaa1d4835371a719893bcf6c5d60c28dcfc3630
Miguel <72183186+Gelium-E@users.noreply.github.com>
2024-12-12 14:35:08 -0800

    Update firebaseConfig.js file
    Update storageBucket

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBpRdNjtxVGiC6NCjt58-gnJUdvnbODXpc",
   authDomain: "carjourney491b.firebaseapp.com",
   projectId: "carjourney491b",
-  storageBucket: "carjourney491b.appspot.com",
+  storageBucket: "carjourney491b.firebasestorage.app",
   messagingSenderId: "248223244957",
   appId: "1:248223244957:web:4b1d289950719d829d6a74",
   measurementId: "G-WF2R4YV2CS"
